718e764fef6dbbda6467c7dba73b519b1f5dddc5
Ayrus <suryaaprakashrj@gmail.com>
2023-08-01 13:37:47 +0530

    button logic added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import { useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { dm_serif_display, bebas_neue, dm_serif_text } from "@/utils/fonts";
 
 export default function Page() {
   const [selectedButton, setSelectedButton] = useState("");
-  const [email, setEmail] = useState("")
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
 
   function handleClick(button: string) {
     setSelectedButton(button);
@@ -15,15 +18,17 @@ export default function Page() {
 
   function handleEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
     if (selectedButton === "") {
-      alert("Please select either 'Influencer' or 'Brand' before entering your email.");
-    }
-     else {
+      alert(
+        "Please select either 'Influencer' or 'Brand' before entering your email."
+      );
+    } else {
       setEmail(e.target.value);
     }
   }
 
   const handleSubmit = async (e: any) => {
-    const { data } = await axios.post(
+    setLoading(true);
+    const response = await axios.post(
       "/api/send",
       {
         email: email,
@@ -34,9 +39,34 @@ export default function Page() {
           "Content-Type": "application/json",
         },
       }
-    )
-    console.log(data);
-    window.location.reload()
+    );
+    if (response.status === 200) {
+      toast.success(
+        " Thank you for signing up for the beta. We will get back to you soon.",
+        {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        }
+      );
+    } else {
+      toast.error("Something went wrong. Please try again.", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+    setLoading(false);
   };
   return (
     <div className="bg-betabg sm:h-full md:h-screen">
@@ -56,14 +86,14 @@ export default function Page() {
           </a>
           <div className="flex md:order-2">
             <a href="https://gomonetize.in">
-            <button
-              type="button"
-              className="text-white bg-black hover:bg-gray-800  font-medium rounded-[4px] text-[20px] px-[30px] py-[10px] text-center"
-            >
-              <span className={bebas_neue.className}>Home</span>
-            </button>
+              <button
+                type="button"
+                className="text-white bg-black hover:bg-gray-800  font-medium rounded-[4px] text-[20px] px-[30px] py-[10px] text-center"
+              >
+                <span className={bebas_neue.className}>Home</span>
+              </button>
             </a>
-            
+
             {/* <button
               data-collapse-toggle="navbar-sticky"
               type="button"
@@ -141,37 +171,71 @@ export default function Page() {
               Redefining the way of influencer marketing
             </span>
           </div>
-          <div className="space-y-[20px]" >
+          <div className="space-y-[20px]">
             <div className="flex space-x-[15px]">
               <button
                 type="button"
                 onClick={() => handleClick("influencer")}
-                className={`rounded-[4px] border-black border-2 text-lg py-[12px] text-center w-[135px] tracking-wide ${selectedButton === "influencer" ? "text-white bg-black" : " bg-transparent text-black border-black border-2"}`}
+                className={`rounded-[4px] border-black border-2 text-lg py-[12px] text-center w-[135px] tracking-wide ${
+                  selectedButton === "influencer"
+                    ? "text-white bg-black"
+                    : " bg-transparent text-black border-black border-2"
+                }`}
               >
                 <span className={bebas_neue.className}>Influencer</span>
               </button>
               <button
                 type="button"
                 onClick={() => handleClick("brand")}
-                className={`rounded-[4px] border-black border-2 text-lg py-[12px] text-center w-[135px] tracking-wide ${selectedButton === "brand" ? "text-white bg-black" : " bg-transparent text-black border-black border-2"}`}
+                className={`rounded-[4px] border-black border-2 text-lg py-[12px] text-center w-[135px] tracking-wide ${
+                  selectedButton === "brand"
+                    ? "text-white bg-black"
+                    : " bg-transparent text-black border-black border-2"
+                }`}
               >
                 <span className={bebas_neue.className}>Brand</span>
               </button>
             </div>
             <div className="w-auto lg:w-[500px]">
-            <input type="email" id="email" onChange={handleEmailChange}
-             className="bg-gray-50 border-2 border-solid border-black text-gray-900 text-sm rounded-[4px] focus:ring-0 focus:outline-none focus:ring-black focus:border-black  block w-full p-[10px] placeholder-gray-600 focus:placeholder-gray-500 placeholder:text-center placeholder:text-lg tracking-wide
-            " placeholder="ENTER YOUR EMAIL ADDRESS" required/>
-            
+              <input
+                type="email"
+                id="email"
+                onChange={handleEmailChange}
+                className="bg-gray-50 border-2 border-solid border-black text-gray-900 text-sm rounded-[4px] focus:ring-0 focus:outline-none focus:ring-black focus:border-black  block w-full p-[10px] placeholder-gray-600 focus:placeholder-gray-500 placeholder:text-center placeholder:text-lg tracking-wide
+            "
+                placeholder="ENTER YOUR EMAIL ADDRESS"
+                required
+              />
             </div>
             <div className="w-auto lg:w-[500px]">
               <button
                 type="button"
-                onClick={(e)=>handleSubmit(e)}
-                className="text-white bg-black hover:bg-gray-800 rounded-[4px] font-medium text-lg px-[10px] py-[10px] text-center w-full tracking-wide"
+                onClick={(e) => handleSubmit(e)}
+                disabled={loading} // Disable the button when loading
+                className={`text-white bg-black hover:bg-gray-800 rounded-[4px] font-medium text-lg px-[10px] py-[10px] text-center w-full tracking-wide ${
+                  loading ? "opacity-50 cursor-not-allowed" : ""
+                }`}
               >
-                <span className={bebas_neue.className}>Join the waitlist</span>
+                {loading ? (
+                  <span>Sending...</span>
+                ) : (
+                  <span className={bebas_neue.className}>
+                    Join the waitlist
+                  </span>
+                )}
               </button>
+              <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+              />
             </div>
           </div>
         </div>
